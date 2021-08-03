272a8cc934fca5019df7d27113037d3d2762b590
tonagalata <tonagalata@gmail.com>
2021-08-02 20:26:09 -0400

    update templates

--- src/cms/preview-templates/TesterPagePreview.js
+++ src/cms/preview-templates/TesterPagePreview.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { TesterTemplate } from '../../templates/tester-page'
+import { TesterPagePreview } from '../../templates/tester-page'
 
-const TesterPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return (
-      <TesterTemplate
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
-}
+const TesterPagePreview = ({ entry, widgetFor }) => (
+  <TesterPagePreview
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
 
 TesterPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 }
 
 export default TesterPagePreview
+
